4b8900c7f76a27600cfae5fe01fb479dd581e637
Daniel Antelo <danielanteloagra@gmail.com>
2018-08-20 12:08:41 +0100

    refactor and add firebase edition

--- node-api/index.js
+++ node-api/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 const api = require('./routes/api.js')
 const status = require('./routes/status.js')
 
 const app = express()
+app.use(cors({ origin: true }));
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
